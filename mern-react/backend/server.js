82a555568387afad9ef96261f89ec102b8ff7f74
raxemmohamed <raxemmohamed@gmail.com>
2019-10-19 23:44:47 -0500

    react mern app

--- mern-react/backend/server.js
+++ mern-react/backend/server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
