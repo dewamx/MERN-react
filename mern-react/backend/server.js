986d33bf2f142764368033d4dd613c24e89693ac
raxemmohamed <raxemmohamed@gmail.com>
2019-10-23 19:38:13 -0500

    MERN-React

--- mern-react/backend/server.js
+++ mern-react/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -23,6 +24,14 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`); 
