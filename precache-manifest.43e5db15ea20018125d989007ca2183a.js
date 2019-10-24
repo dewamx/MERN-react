c2b3f59761fa7d6ae440f8d324948770c34df99f
raxemmohamed <raxemmohamed@gmail.com>
2019-10-23 19:03:47 -0500

    Updates

---
+++ precache-manifest.43e5db15ea20018125d989007ca2183a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2895f81811a4b2e40ecac70837f02d6",
+    "url": "/MERN-react/index.html"
+  },
+  {
+    "revision": "ecedec20f0f292aaeac3",
+    "url": "/MERN-react/static/css/2.189d53d3.chunk.css"
+  },
+  {
+    "revision": "ecedec20f0f292aaeac3",
+    "url": "/MERN-react/static/js/2.160020cd.chunk.js"
+  },
+  {
+    "revision": "edc1e160e425124b0a20",
+    "url": "/MERN-react/static/js/main.90ab14a7.chunk.js"
+  },
+  {
+    "revision": "dd579211084185c5a9ed",
+    "url": "/MERN-react/static/js/runtime-main.b52a1510.js"
+  }
+]);
